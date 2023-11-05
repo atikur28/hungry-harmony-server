9a3818533a2d848a4a1061a97649e8239aadeb67
atikur28 <atikur45683@gmail.com>
2023-11-06 02:01:18 +0600

    Single Food details getting complete

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -36,6 +36,13 @@ async function run() {
       .skip(page * size)
       .limit(size)
       .toArray();
+      res.send(result);
+    })
+
+    app.get('/foods/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await foodsCollection.findOne(query);
       res.send(result);
     })
 
