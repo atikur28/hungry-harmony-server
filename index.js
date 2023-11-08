e962b6bfa1a1c0b7e530addc08a41af8327db32b
atikur28 <atikur45683@gmail.com>
2023-11-08 11:36:42 +0600

    Vercel deploy complete

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      'https://hungry-harmony.web.app/',
+      'https://hungry-harmony.firebaseapp.com'
+    ],
     credentials: true,
   })
 );
@@ -50,7 +53,7 @@ const verifyToken = (req, res, next) => {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const foodsCollection = client.db("foodsData").collection("foods");
     const orderedCollection = client.db("foodsData").collection("orderedFoods");
@@ -181,7 +184,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
